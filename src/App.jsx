047a1dd91ebd22e27b6a1ca8093ab63c4b6e43a7
ysmk <0620komako@gmail.com>
2025-10-01 21:11:51 +0900

    feat:props追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import "./App.css";
 import Button from "./Button"
 
 function App() {
+
+    const handleClick = () => {
+        alert("クリックされました");
+    }
+
     return (
         <>
             <h1>Hello World</h1>
-            <Button />
+            <Button type='submit' disabled={false} onClick={handleClick}>
+                ボタンクリック
+            </Button>
         </>
     );
 }
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,9 +1,11 @@
-function Button () {
-    const handleclick = () => {
-        alert("クリックされました");
-    }
+function Button(props) {
+    const { type, disabled ,children ,onClick} = props;
 
-    return <button type="button" onClick={handleclick}>クリック</button>
+    return (
+        <button type={type} disabled={disabled} onClick={onClick}>
+            {children} {/*コンポーネントの中身に書いた要素やテキストを受け取るためのもの*/}
+        </button>
+    );
 }
 
 export default Button;
