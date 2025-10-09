fe608a02082919af62be0e6ecc73cf871efc536d
ysmk <0620komako@gmail.com>
2025-10-09 20:36:04 +0900

    fix: Hooks(useState)のためにonClickを削除して整理

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,10 @@ import Button from "./components/Button/Button"
 
 function App() {
 
-    const handleClick = () => {
-        alert("クリックされました");
-    }
-
     return (
         <>
             <h1>Hello World</h1>
-            <Button type='submit' disabled={false} onClick={handleClick}>
+            <Button type='submit' disabled={false}>
                 ボタンクリック
             </Button>
         </>
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import styles from './Button.module.css';
 
 function Button(props) {
-    const { type, disabled ,children ,onClick} = props;
+    const { type, disabled ,children } = props;
 
     return (
-        <button className={styles.button} type={type} disabled={disabled} onClick={onClick}>
+        <button className={styles.button} type={type} disabled={disabled} >
             {/* CSSモジュールのButton.module.cssで設定したクラス（.button）を読み込む。ReactではclassではなくclassName。{importした変数(styles).クラス名(button)} */}
             {children} {/*コンポーネントの中身に書いた要素やテキストを受け取るためのもの*/}
         </button>
